5df6a80854e10cf11b276b89eae862de7a9361af
Alexandre-Mattos-Cordeiro <alexandre@imobia.app>
2023-01-23 11:42:37 -0300

    Adiciona um novo retorno de erros para a validação de código 400

--- src/Adapter/GuzzleHttpAdapter.php
+++ src/Adapter/GuzzleHttpAdapter.php
@@ -151,7 +151,8 @@ class GuzzleHttpAdapter implements AdapterInterface
         $content = json_decode($body);
 
         if ($code === 400) {
-            throw new ValidationException(isset($content->message) ? $content->message : $code . ' Dados inválidos.', $code, $content->errors);
+            $error = new ValidationException(isset($content->message) ? $content->message : $code . ' Dados inválidos.', $code, $content->errors);
+            throw $error->getErrors();
         } else if ($code === 403) {
             throw new ForbiddenException(isset($content->message) ? $content->message : $code . ' Operação não permitida.', $code, $content->errors);
         } else {
