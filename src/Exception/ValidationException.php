d5f2c5a6859b57326449e2a79dfc4b71a631c90a
Alexandre-Mattos-Cordeiro <alexandre@imobia.app>
2023-01-23 15:42:59 -0300

    Corrige o construct

--- src/Exception/ValidationException.php
+++ src/Exception/ValidationException.php
@@ -8,12 +8,11 @@ class ValidationException extends HttpException
 
     public function __construct($error_message, $reponse_code, $errors)
     {
+        parent::__construct($reponse_code . ' ' . $error_message, $reponse_code);
+
         $this->errors = array_map(function ($error) {
             return $error->description;
         }, $errors);
-
-        parent::__construct($this->getErrors(), $reponse_code);
-
     }
 
     public function getErrors()
