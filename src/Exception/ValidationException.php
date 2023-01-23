177c7bac3038db9279c0904e990903a74569aa1b
Alexandre-Mattos-Cordeiro <alexandre@imobia.app>
2023-01-23 15:25:46 -0300

    Altera a validação do envio do objeto

--- src/Exception/ValidationException.php
+++ src/Exception/ValidationException.php
@@ -8,13 +8,11 @@ class ValidationException extends HttpException
 
     public function __construct($error_message, $reponse_code, $errors)
     {
+        parent::__construct($response_code . ' ' . $error_message, $reponse_code);
 
         $this->errors = array_map(function ($error) {
             return $error->description;
         }, $errors);
-
-        parent::__construct($response_code . ' ' . $error_message, $reponse_code, $this->getErrors());
-
     }
 
     public function getErrors()
