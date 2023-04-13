126278f82ad1aa4a34a02933c63160941c3ff1ef
Alexandre-Mattos-Cordeiro <alexandre@imobia.app>
2023-04-13 11:54:42 -0300

    Create the endpoint to get all bankAccounts and the endpoint to delete them

--- src/Api/BankAccount.php
+++ src/Api/BankAccount.php
@@ -17,6 +17,13 @@ class BankAccount extends \Imobia\Asaas\Api\AbstractApi
         return new BankAccountEntity(json_decode($balance));
     }
 
+    public function getAllAccounts()
+    {
+        $balance = $this->adapter->get(sprintf('%s/bankAccounts', $this->endpoint));
+
+        return new BankAccountEntity(json_decode($balance));
+    }
+
     /**
      * Creates new BankAccount
      *
@@ -26,6 +33,21 @@ class BankAccount extends \Imobia\Asaas\Api\AbstractApi
     public function create(array $data)
     {
         $bankAccount = $this->adapter->post(sprintf('%s/bankAccounts/mainAccount', $this->endpoint), $data);
+
+        $bankAccount = json_decode($bankAccount);
+
+        return new BankAccountEntity($bankAccount);
+    }
+
+    /**
+     * Delete a BankAccount
+     *
+     * @param   array  $data  BankAccount Data
+     * @return  BankAccountEntity
+     */
+    public function delete($id)
+    {
+        $bankAccount = $this->adapter->delete(sprintf('%s/bankAccounts?id=', $this->endpoint, $id));
 
         $bankAccount = json_decode($bankAccount);
 
