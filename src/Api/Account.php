5bd6ebdaf08bee78144019c44853e35a5cd149b1
vitor <vitor@imobia.app>
2024-05-06 10:38:10 -0300

    adição de busca de accounts

--- src/Api/Account.php
+++ src/Api/Account.php
@@ -39,4 +39,23 @@ class Account extends \Imobia\Asaas\Api\AbstractApi
 
         return json_decode($account);
     }
+
+    /**
+     * Get all accounts
+     *
+     * @param   array  $filters  (optional) Filters Array
+     * @return  array  Accounts Array
+     */
+    public function getAll(array $filters = [])
+    {
+        $accounts = $this->adapter->get(sprintf('%s/accounts?%s', $this->endpoint, http_build_query($filters)));
+
+        $accounts = json_decode($accounts);
+
+        $this->extractMeta($accounts);
+
+        return array_map(function ($account) {
+            return new AccountEntity($account);
+        }, $accounts->data);
+    }
 }
