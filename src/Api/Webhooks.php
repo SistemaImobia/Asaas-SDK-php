4b133fa53d4cd4010fcfde5cf7d6c71737fd6b08
Marcelo Migliorini <marcelo@imobia.app>
2024-06-06 14:36:42 -0300

    Adiciona novo endpoint de webhooks do Asaas

--- src/Api/Webhooks.php
+++ src/Api/Webhooks.php
@@ -24,24 +24,6 @@ class Webhooks extends \Imobia\Asaas\Api\AbstractApi
         return new WebhooksEntity($webhooks);
     }
 
-    // public function getAllTransfers()
-    // {
-    //     $webhooks = $this->adapter->get(sprintf('%s/webhooks/transfer', $this->endpoint));
-
-    //     $webhooks = json_decode($webhooks);
-
-    //     return new WebhooksEntity($webhooks);
-    // }
-
-    // public function getAllInvoices()
-    // {
-    //     $webhooks = $this->adapter->get(sprintf('%s/webhooks/invoice', $this->endpoint));
-
-    //     $webhooks = json_decode($webhooks);
-
-    //     return new WebhooksEntity($webhooks);
-    // }
-
     /**
      * Creates new Webhooks
      *
@@ -57,21 +39,4 @@ class Webhooks extends \Imobia\Asaas\Api\AbstractApi
         return new WebhooksEntity($webhooks);
     }
 
-    // public function createTransfer(array $data)
-    // {
-    //     $webhooks = $this->adapter->post(sprintf('%s/webhooks/transfer', $this->endpoint), $data);
-
-    //     $webhooks = json_decode($webhooks);
-
-    //     return new WebhooksEntity($webhooks);
-    // }
-
-    // public function createInvoice(array $data)
-    // {
-    //     $webhooks = $this->adapter->post(sprintf('%s/webhooks/invoice', $this->endpoint), $data);
-
-    //     $webhooks = json_decode($webhooks);
-
-    //     return new WebhooksEntity($webhooks);
-    // }
 }
