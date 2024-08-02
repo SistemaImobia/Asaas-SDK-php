0d11a3f6a18232627aea91ad883ad786f10e4f38
Marcelo Migliorini <marcelo@imobia.app>
2024-08-02 18:00:46 -0300

    Modifica função do WebHook para buscar todos os dados do mesmo

--- src/Api/Webhooks.php
+++ src/Api/Webhooks.php
@@ -21,7 +21,14 @@ class Webhooks extends \Imobia\Asaas\Api\AbstractApi
 
         $webhooks = json_decode($webhooks);
 
-        return new WebhooksEntity($webhooks);
+        // return new WebhooksEntity($webhooks);
+
+        $this->extractMeta($webhooks);
+
+        return array_map(function($webhooks)
+        {
+            return new WebhooksEntity($webhooks);
+        }, $webhooks->data);
     }
 
     /**
