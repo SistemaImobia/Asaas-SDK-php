8b6b9166697364a84fca675dc456c31a45225868
vitor-dev <vitor@imobia.app>
2024-08-28 14:31:08 -0300

    .

--- src/Api/MyAccount.php
+++ src/Api/MyAccount.php
@@ -43,6 +43,37 @@ class MyAccount extends \Imobia\Asaas\Api\AbstractApi
 
     public function createDocument(array $data, string $id)
     {
+        if (array_key_exists('documentFile', $data)) {
+            $multipartData = [];
+
+            foreach ($data as $key => $value) {
+                if ($value instanceof \Illuminate\Http\UploadedFile) {
+                    $multipartElement = [
+                        'name'     => $key,
+                        'contents' => fopen($value->getPathname(), 'r'),
+                        'filename' => $value->getClientOriginalName(),
+                        'headers'  => ['Content-Type' => $value->getMimeType()],
+                    ];
+                } elseif (is_resource($value)) {
+                    $multipartElement = [
+                        'name'     => $key,
+                        'contents' => $value,
+                    ];
+                } else {
+                    $multipartElement = [
+                        'name'     => $key,
+                        'contents' => is_bool($value) ? ($value ? 'true' : 'false') : $value,
+                    ];
+                }
+
+                $multipartData[] = $multipartElement;
+            }
+
+            $info = $this->adapter->post(sprintf('%s/myAccount/documents/' . $id, $this->endpoint), $multipartData, 'multipart');
+
+            return json_decode($info);
+        }
+
         $documents = $this->adapter->post(sprintf('%s/myAccount/documents/' . $id, $this->endpoint), $data);
 
         return $documents;
