1f33ed5957518067e8dd4e13cb4dcf71bfa11f2b
Marcelo Migliorini <marcelo@imobia.app>
2024-10-03 10:16:01 -0300

    Adiciona id de conta para recuperação

--- src/Entity/Account.php
+++ src/Entity/Account.php
@@ -12,6 +12,11 @@ final class Account extends \Imobia\Asaas\Entity\AbstractEntity
     /**
      * @var string
      */
+    public $id;
+
+    /**
+     * @var string
+     */
     public $name;
 
     /**
